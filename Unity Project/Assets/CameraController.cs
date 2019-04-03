f8ae9dacfd4e7de870be267c1bdec281ce0b4c55
halimt92 <48407395+halimt92@users.noreply.github.com>
2019-04-03 13:25:57 +0300

    Initial Commit

---
+++ Unity Project/Assets/CameraController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public Transform finalposTransform;
+    private Vector3 finalpos;
+    private Vector3 initialpos;
+    private float rate=2f;
+    private bool positionChanged;
+	// Use this for initialization
+	void Start () {
+        initialpos = transform.position;
+        finalpos = finalposTransform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            if (!positionChanged)
+            {
+                StartCoroutine(ChangePosition(initialpos, finalpos));
+                positionChanged = true;
+                Vector3 temp = finalpos;
+                finalpos = initialpos;
+                initialpos = temp;
+            }
+        }
+
+	}
+
+    IEnumerator ChangePosition(Vector3 initial, Vector3 final)
+    {
+        float timeElapsed = 0;
+
+        while (timeElapsed<1)
+        {
+            timeElapsed += Time.deltaTime;
+            transform.position = Vector3.Slerp(initial, final,timeElapsed*rate);
+            yield return null;
+        }
+        positionChanged = false;
+
+    }
+
+
+}
