a25dfeff4a747753dd251fc1638e58daf99f804d
halimt92 <48407395+halimt92@users.noreply.github.com>
2019-04-12 12:58:41 +0300

    Minimap

---
+++ Unity Project/Assets/MapCamController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCamController : MonoBehaviour {
+
+    public GameObject player;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.transform.position.x, transform.position.y, player.transform.position.z);
+        transform.rotation = Quaternion.Euler(new Vector3(transform.rotation.eulerAngles.x,player.transform.rotation.y, transform.rotation.eulerAngles.z));
+	}
+}
